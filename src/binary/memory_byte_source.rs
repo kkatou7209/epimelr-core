30e7f0f0279b03e3dc77c142e22f469ba5bef40e
kkatou7209 <kkatou7209@gmail.com>
2026-01-15 01:35:48 +0900

    define minimal lib api

--- src/binary/memory_byte_source.rs
+++ src/binary/memory_byte_source.rs
@@ -1,8 +1,17 @@
-use std::ops::Index;
+use std::ops::{Index, Range, RangeFull};
+use std::process::Output;
 
 /// A byte source backed by an in-memory vector of bytes.
+#[derive(Debug, Clone)]
 pub struct MemoryByteSource {
     data: Vec<u8>,
+}
+
+impl MemoryByteSource {
+    /// Creates a new `MemoryByteSource` from the given vector of bytes.
+    pub fn new(data: Vec<u8>) -> Self {
+        Self { data }
+    }
 }
 
 impl Index<usize> for MemoryByteSource {
@@ -11,6 +20,24 @@ impl Index<usize> for MemoryByteSource {
 
     fn index(&self, index: usize) -> &u8 {
         &self.data[index]
+    }
+}
+
+impl Index<Range<usize>> for MemoryByteSource {
+
+    type Output = [u8];
+
+    fn index(&self, index: Range<usize>) -> &Self::Output {
+        &self.data[index]
+    }
+}
+
+impl Index<RangeFull> for MemoryByteSource {
+
+    type Output = [u8];
+
+    fn index(&self, _index: RangeFull) -> &Self::Output {
+        &self.data[..]
     }
 }
 
---
+++ src/redactor.rs
@@ -0,0 +1,87 @@
+//! This module contains the core implementation of the redactor library.
+use std::{fs::File, sync::Arc};
+
+use crate::binary::*;
+
+/// A core implementation of this library.
+/// <br>
+/// 
+/// For instanciating, use `Redactor::from(&File)`.
+/// ```rs
+/// use std::fs::File;
+/// use redactor::Redactor;
+/// 
+/// let file = File::open("path/to/file").expect("Failed to open file");
+/// let redactor = Redactor::from(&file);
+/// ```
+/// <br>
+/// 
+/// You can also create a `Redactor` instance from a file path string:
+/// 
+/// ```rs
+/// use redactor::Redactor;
+/// 
+/// let redactor = Redactor::from("path/to/file");
+/// ```
+/// 
+/// To get the length of the content, use the `content_len` method:
+/// 
+/// ```rs
+/// let length: usize = redactor.content_len();
+/// println!("Content length: {}", length);
+/// ```
+/// <br>
+/// 
+/// To get the content as a `String`, use the `as_string` method:
+/// 
+/// ```rs
+/// let content: String = redactor.as_string();
+/// println!("Content: {}", content);
+/// ```
+#[derive(Debug, Clone)]
+pub(crate) struct Redactor {
+    /// The original byte source.
+    original: Arc<dyn ByteSource>,
+}
+
+impl Redactor {
+    
+    /// Returns the length of the content.
+    pub fn content_len(&self) -> usize {
+        self.original.len()
+    }
+
+    /// Returns the content as a `String`.
+    pub fn as_string(&self) -> String {
+
+        let original = Arc::clone(&self.original);
+        
+        String::from_utf8_lossy(&original[..]).to_string()
+    }
+}
+
+impl From<&File> for Redactor {
+
+    fn from(file: &File) -> Self {
+        let source = create_source_from_file(file)
+            .expect("Failed to create byte source from file");
+
+        Self {
+            original: Arc::new(source),
+        }
+    }
+}
+
+impl From<&str> for Redactor {
+    
+    fn from(value: &str) -> Self {
+        let file = File::open(value).expect("Failed to open file");
+
+        let source = create_source_from_file(&file)
+            .expect("Failed to create byte source from file");
+
+        Self {
+            original: Arc::new(source),
+        }
+    }
+}
